710949a072d6e5b0c23a01ae427b54d35303b767
Aytac Allahverdiyeva <aytac.allahverdiyeva01@gmail.com>
2024-06-11 21:02:30 +0400

    added some changes

---
+++ src/utils/data.js
@@ -0,0 +1,23 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        // icon: 
+        skills: [
+            {skill: "HTML5", percentage: "90%"},
+            {skill: "CSS (Sass/SCSS)", percentage: "90%"},
+            {skill: "JavaScript", percentage: "70%"},
+            {skill: "React.js", percentage: "75%"},
+        ],
+    },
+    {
+        title: "Tools",
+        // icon: 
+        skills: [
+            {skill: "Git & Github", percentage: "70%"},
+            {skill: "Visual Studio Code", percentage: "90%"},
+            {skill: "Webpack", percentage: "70%"},
+            {skill: "Responsive Design", percentage: "75%"},
+        ],
+    },
+    
+]
